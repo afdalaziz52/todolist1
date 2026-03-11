19bcaee964d33bc1ec5fb9c1f3ea965731917d90
mhmdgthan <gathanprayoga6@gmail.com>
2026-03-11 16:46:20 +0700

    Update task.go

--- models/task.go
+++ models/task.go
@@ -3,19 +3,20 @@ package models
 import "time"
 
 type Task struct {
-    ID             int       `json:"id"`
-    UserID         int       `json:"user_id"`
-    Title          string    `json:"title"`
-    Category       string    `json:"category"`
-    CustomCategory *string   `json:"custom_category"` 
-    Status         string    `json:"status"`
-    CreatedAt      time.Time `json:"created_at"`
-    UpdatedAt      time.Time `json:"updated_at"`
+    ID             int        `json:"id"`
+    UserID         int        `json:"user_id"`
+    Title          string     `json:"title"`
+    Category       string     `json:"category"`
+    CustomCategory *string    `json:"custom_category"`
+    Status         string     `json:"status"`
+    Deadline       *time.Time `json:"deadline"`       
+    CreatedAt      time.Time  `json:"created_at"`
+    UpdatedAt      time.Time  `json:"updated_at"`
 }
 
-// untuk request tambah & edit task
 type TaskRequest struct {
-    Title          string  `json:"title"           validate:"required"`
-    Category       string  `json:"category"        validate:"required,oneof=work study personal finance social other"`
-    CustomCategory *string `json:"custom_category"`
+    Title          string     `json:"title"           validate:"required"`
+    Category       string     `json:"category"        validate:"required,oneof=work study personal finance social other"`
+    CustomCategory *string    `json:"custom_category"`
+    Deadline       *time.Time `json:"deadline"`        
 }
