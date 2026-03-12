d39bd975621b1b149c6e4b3ca42a8cd696150dc7
mhmdgthan <gathanprayoga6@gmail.com>
2026-03-12 15:09:31 +0700

    Update task.go

--- models/task.go
+++ models/task.go
@@ -7,16 +7,18 @@ type Task struct {
     UserID         int        `json:"user_id"`
     Title          string     `json:"title"`
     Category       string     `json:"category"`
+    Priority       string     `json:"priority"`
     CustomCategory *string    `json:"custom_category"`
     Status         string     `json:"status"`
-    Deadline       *time.Time `json:"deadline"`       
+    Deadline       *time.Time `json:"deadline"`
     CreatedAt      time.Time  `json:"created_at"`
     UpdatedAt      time.Time  `json:"updated_at"`
 }
 
 type TaskRequest struct {
-    Title          string     `json:"title"           validate:"required"`
-    Category       string     `json:"category"        validate:"required,oneof=work study personal finance social other"`
+    Title          string     `json:"title"     validate:"required"`
+    Category       string     `json:"category"  validate:"required,oneof=work study personal finance social other"`
+    Priority       string     `json:"priority"  validate:"required,oneof=low medium high"`
     CustomCategory *string    `json:"custom_category"`
-    Deadline       *time.Time `json:"deadline"`        
+    Deadline       *time.Time `json:"deadline"`
 }
