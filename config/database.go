f251d885d3b49566b0d5ff28861f0d2b4222d46c
mhmdgthan <gathanprayoga6@gmail.com>
2026-03-16 11:47:16 +0700

    Update database.go

--- config/database.go
+++ config/database.go
@@ -1,36 +1,27 @@
-package config
-
+ini masih pake yang sebelumnya ya package config
 import (
     "database/sql"
     "fmt"
     "log"
     "os"
-
-    _ "github.com/lib/pq"
+    _ "github.com/go-sql-driver/mysql"
     "github.com/go-playground/validator/v10"
 )
-
 var DB *sql.DB
 var Validate *validator.Validate
-
 func InitDB() {
     var err error
-    dsn := os.Getenv("DATABASE_URL")
-
-    DB, err = sql.Open("postgres", dsn)
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+        os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"),
+        os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+    DB, err = sql.Open("mysql", dsn)
     if err != nil {
         log.Fatal(err)
     }
-
     if err = DB.Ping(); err != nil {
         log.Fatal(err)
     }
-
     Validate = validator.New()
-
     fmt.Println("✅ Database connected!")
+    fmt.Printf("   Database: %s\n", os.Getenv("DB_NAME"))
 }
-
-lalu jalankan: 
-go get github.com/lib/pq
-go run main.go
